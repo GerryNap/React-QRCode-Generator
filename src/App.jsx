421df39da217767ac9df5440d8ac93cb18d12c39
GerryNap <gerry.nap98@gmail.com>
2022-08-03 12:25:04 +0200

    Complete project

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import QRCode from 'qrcode';
+import { useState } from 'react';
+
+function App() {
+  const [url, setUrl] = useState('');
+  const [qrcode, setQrcode] = useState('');
+
+  const GenerateQRCode = () =>{
+    QRCode.toDataURL(url, {
+      width: 800,
+      margin: 1,
+      color:{
+        dark: '#000000ff',
+        light: '#ffffffff'
+      }
+    }, (err, url) => {
+      if(err) return console.error(err);
+      setQrcode(url);
+    })
+  }
+
+  return (
+    <div className="app">
+      <h1>QR Code Generator</h1>
+      <input 
+        type="text" 
+        placeholder="https:/google.com"
+        value={url}
+        onChange={(evt) => setUrl(evt.target.value)}/>
+      <button onClick={GenerateQRCode}>Generate</button>
+      {qrcode && <>
+        <img src={qrcode} alt=""/>
+        <a href={qrcode} download="qrcode.png">Download</a>
+      </>}
+    </div>
+  );
+}
+
+export default App;
